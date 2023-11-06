934224cf100dd7433a798d293a34a7ba19e2ef0e
‘Elena <tcachelena@gmail.com>
2023-11-06 18:43:28 +0600

    feat: Created cards on the main page using the template tag.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 'use strict;'
 // import { filterFunction } from './_search';
+import { indexCards } from './_consts';
 import { showBlock } from './_utils';
+import { renderCardList } from './_cards';
 
 showBlock(); 
+renderCardList(indexCards);
