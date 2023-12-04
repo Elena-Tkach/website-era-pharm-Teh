8f6e7e4d7701731447d6083cdfbe0189cfe05da6
‘Elena <tcachelena@gmail.com>
2023-12-04 10:16:50 +0600

    fix: Fixed bugs in styles

--- src/js/_utils.js
+++ src/js/_utils.js
@@ -5,28 +5,21 @@ export const showBlock = () => {
   const blockMoreEl = document.querySelector('.js-block-more');
   const navBlockEl = document.querySelector('.js-nav-block');
   const closeMenuBtnEl = document.querySelector('.js-btn-close');
-  const listIconst = document.querySelectorAll('.js-btn-icon');
-  const cardContainer = document.querySelector('.js-cards-contaier');
-  const overlay = document.querySelector('.js-overlay');
+  // const overlay = document.querySelector('.js-overlay');
 
 
   const toggleAccordeon = (moreBtn, block) => {
     block.classList.toggle('show-slider');
-    console.log(block);
-   
-    
     
     if (block.classList.contains('show-slider')) {
       block.setAttribute('aria-hidden', 'false');
       moreBtn.setAttribute('aria-label', 'Закрыть блок');
       moreBtn.innerHTML = 'Скрыть блок';
-    }
-    if (!block.classList.contains('show-slider')) {
-      block.setAttribute('aria-hidden', 'true');
+    } else {
+       block.setAttribute('aria-hidden', 'true');
       moreBtn.setAttribute('aria-label', 'Показать больше информации');
       moreBtn.innerHTML = 'Больше информации';
-    }
-    
+    }  
   };
 
 
@@ -34,6 +27,7 @@ export const showBlock = () => {
   const showMenu = (element, closeMenuBtn) => {
     element.classList.add('show'); 
     previousActiveElement = document.activeElement;
+    element.setAttribute('aria-hidden', 'false');
 
      setTimeout(() => {
       closeMenuBtn.focus();
@@ -43,14 +37,9 @@ export const showBlock = () => {
 
   const hideMenu = (element) => {
     element.classList.remove('show'); 
+    element.setAttribute('aria-hidden', 'true');
     previousActiveElement.focus();
-  }
-
-  const desactiveBtn = (list) => {
-    console.log(list);
-    list.forEach(item => {
-      item.classList.remove('selected');
-    });
+     
   }
 
   document.body.addEventListener('click', event => {
@@ -61,43 +50,30 @@ export const showBlock = () => {
 
     if (event.target.classList.contains('js-btn-menu-open')) {
       showMenu(navBlockEl, closeMenuBtnEl);
-      overlay.classList.add('show');
+      // overlay.classList.add('show');
      
     }
 
     if (event.target.classList.contains('js-btn-close')) {
       hideMenu(navBlockEl);
-      overlay.classList.remove('show');
+      // overlay.classList.remove('show');
     }
 
     if (event.target.classList.contains('js-overlay')) {
-      
-       hideMenu(navBlockEl);
+      hideMenu(navBlockEl);
+      // overlay.classList.remove('show');
     }
 
     if (event.target.classList.contains('cards__article')) {
-    
+      console.log(event.target);
       showMenu(modal, closeMenuBtnEl);
       document.body.classList.add('no-scroll');
     }
 
-    if (event.target.classList.contains('js-btn-close') ||
-        event.target.classList.contains('modal')  ) {
-       
+    if (event.target.classList.contains('js-btn-modal-close') ||
+        event.target.classList.contains('modal')) {
       hideMenu(modal);
       document.body.classList.remove('no-scroll');
-    }
-
-    if (event.target.classList.contains('btn--icons-list')) {
-        desactiveBtn(listIconst);
-      event.target.classList.add('selected');
-      cardContainer.classList.add('card-list');
-    }
-
-    if (event.target.classList.contains('btn--icons-cards')) {
-      desactiveBtn(listIconst);
-      event.target.classList.add('selected');
-       cardContainer.classList.remove('card-list');
     }
   })
 };
