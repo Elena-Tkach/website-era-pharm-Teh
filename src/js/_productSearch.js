e312cfcc60f4c74808082177d17cd0ba8a9dada4
‘Elena <tcachelena@gmail.com>
2023-12-08 14:22:47 +0600

    fix: Search by product

--- src/js/_productSearch.js
+++ src/js/_productSearch.js
@@ -1,7 +1,18 @@
 const searchInput = document.querySelector('.js-search');
 const searchList = document.querySelector('.js-product-search');
 const searchItems = Array.from(document.querySelectorAll('.js-search-link'));
+const searchDots = document.querySelector('.search__dots');
 
+const textContent = (element, style, text) => {
+  if (style) {
+    element.style.cssText = `${style}`;
+  }
+  if (text) {
+    element.innerHTML = `${text}`;
+  }
+
+  
+}
 const filterProduct = () => {
   searchInput.addEventListener('input', () => {
     const searchText = searchInput.value.toLowerCase();
@@ -18,17 +29,18 @@ const filterProduct = () => {
 
     if (filteredItems.length > 2) {
       searchList.classList.add('scrolling');
-      document.querySelector('.search__dots').style.cssText = 'visibility: hidden; opacity: 0; transition: 0.6s';
+      textContent(searchDots, 'visibility: hidden; opacity: 0; transition: 0.6s;');
     } else {
       searchList.classList.remove('scrolling');
+      textContent(searchDots, 'visibility: visible; opacity: 1;transition: 0.6s;', '...');
     }
 
     if (filteredItems.length < 1) {
-      document.querySelector('.search__dots').style.cssText = 'visibility: visible; opacity: 1; color: blue; font-size: 12px';
-      document.querySelector('.search__dots').innerHTML = 'Ничего не найденно!';
+      textContent(searchDots, 'visibility: visible; opacity: 1; color: blue;', 'Ничего не найденно!');
     }
 
     if (searchText.length === 0) {
+      textContent(searchDots, 'visibility: visible; opacity: 1; transition: 0.6s;', '...');
       searchList.classList.remove('scrolling');
     }
   });
