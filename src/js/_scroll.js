369fb2ab8d10c4f319b1e99e9e1572ce3aa11fdf
‘Elena <tcachelena@gmail.com>
2023-12-13 09:08:04 +0600

    feat: a

--- src/js/_scroll.js
+++ src/js/_scroll.js
@@ -15,22 +15,3 @@ export const scroll = () => {
 };
 
 export default scroll;
-
-// function scrollheader() {
-//     const scrollPosition = () => window.pageXOffset || document.documentElement.scrollTop;
-//     const containHide = () => header.classList.contains(`hide`);
-
-//     if (scrollPosition() >
-//         scroll && !containHide() &&
-//         scrollPosition() > defaultOffset) {
-//         header.classList.add(`hide`);
-
-//     }
-
-//     if (scrollPosition() < scroll && containHide) {
-//         header.classList.remove(`hide`);
-
-//     }
-
-//     scroll = scrollPosition();
-// }
