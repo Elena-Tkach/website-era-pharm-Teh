154f65d084d3ac83ae0fac9c0bd3c02aaaddc439
‘Elena <tcachelena@gmail.com>
2023-12-14 09:54:04 +0600

    fix: Remove spaces

--- src/js/_scroll.js
+++ src/js/_scroll.js
@@ -3,7 +3,7 @@ const btnHeader = document.querySelector('.js-btn-menu-open ');
 export const scroll = () => {
   window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
-    if (scrollPosition >= 50) {
+    if (scrollPosition >= 100) {
       btnHeader.classList.add('has-scrolled');
     } else {
       btnHeader.classList.remove('has-scrolled');
